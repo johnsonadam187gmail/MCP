1657df20e267df3dd5716725db2f472a5e5000c1
adamjohnson187 <johnsonadam187@gmail.com>
2025-03-23 21:56:29 +1300

    updating scripts

--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@ from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("Demo")
 
-
+@mcp.tool()
+def usd_to_gbp(amount: float) -> float:
+  """Convert USD to GBP"""
+  EXCHANGE_RATE = 0.79
+  return round(amount * EXCHANGE_RATE, 2)
 
 
 
 
 if __name__ == "__main__":
-  pass
+  mcp.run()
